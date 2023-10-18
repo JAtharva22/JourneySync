fbda7999f98f58a04ea79c7fb3ff502aca175a89
Atharva J <me.atharvajadhav@gmail.com>
2023-10-18 16:09:53 +0530

    user profile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from './Pages/Login/Login';
+import ProfilePage from './Pages/UserProfile/UserProfile';
+import Footer from './Components/footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <Login/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+      <Footer/>
+    </Router>
   );
 }
 
