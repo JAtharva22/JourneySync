c02e7550a98980bf62550bac77841d2fab92ae6d
surajgupta101 <56082030+surajgupta101@users.noreply.github.com>
2023-11-07 19:19:37 +0530

    navigate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Login from './Pages/Login/Login';
 import ProfilePage from './Pages/UserProfile/UserProfile';
 import Footer from './Components/footer/Footer';
 import Home from './Pages/Home/Home';
 import Navbar from './Components/Navbar/Navbar';
+import { useState } from 'react';
 
 function App() {
+  const [authtoken,setAuthtoken] = useState('')
 
   return (
     <Router>
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={!authtoken ? <Login authtoken ={authtoken} setAuthtoken = {setAuthtoken} /> : <Navigate to="/" />} />
         <Route path="/profile" element={<ProfilePage />} />
       </Routes>
       <Footer />
