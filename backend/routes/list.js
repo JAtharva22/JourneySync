566ae87459f2d87036a2739772709d335f6b0060
Atharva J <me.atharvajadhav@gmail.com>
2023-11-07 22:10:30 +0530

    minor change

--- backend/routes/list.js
+++ backend/routes/list.js
@@ -4,36 +4,42 @@ const fetchuser = require('../middleware/fetchuser');
 const List = require('../models/List');
 const { body, validationResult } = require('express-validator');
 
-// ROUTE 2: Add a new List using: POST "/api/lists/addlist". Login required
+// ROUTE 1: Add a new List using: POST "/api/lists/addlist". Login required
 router.post('/addlist', fetchuser, [
     body('src', 'Enter a valid source'),
     body('dest', 'Enter a valid destination'),
-    ], async (req, res) => {
-        try {
-            const { src, dest } = req.body;
+], async (req, res) => {
+    try {
+        const { src, dest } = req.body;
 
-            // If there are errors, return Bad request and the errors
-            const errors = validationResult(req);
-            if (!errors.isEmpty()) {
-                return res.status(400).json({ errors: errors.array() });
-            }
-            const list = new List({
-                src, dest, userId: req.user.id
-            })
-            const savedNote = await list.save()
-
-            res.json(savedNote)
-
-        } catch (error) {
-            console.error(error.message);
-            res.status(500).send("Internal Server Error");
+        // If there are errors, return Bad request and the errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
         }
-    });
 
-// ROUTE 3: Update an existing List using: PUT "/api/lists/updatelist". Login required
-router.put('/updatelist/:id', fetchuser, async (req, res) => {
+        // Find the list to be created and if found return error
+         const existingList = await List.findOne({ userId: req.user.id });
+        if (existingList) {
+            return res.status(200).send('Resource already exists');
+        }
+
+        const list = new List({
+            src, dest, userId: req.user.id
+        })
+        const savedNote = await list.save()
+
+        res.json(savedNote)
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+// ROUTE 2: Update an existing List using: PUT "/api/lists/updatelist". Login required
+router.put('/updatelist', fetchuser, async (req, res) => {
     const { src, dest } = req.body;
-    console.log('req.body')
     try {
         // Create a newList object
         const newList = {};
@@ -41,14 +47,11 @@ router.put('/updatelist/:id', fetchuser, async (req, res) => {
         if (dest) { newList.dest = dest };
 
         // Find the list to be updated and update it
-        let list = await List.findById(req.params.id);
+        let list = await List.findOne({ userId: req.user.id });
         if (!list) { return res.status(404).send("Not Found") }
 
-        if (list.userId.toString() !== req.user.id) {
-            return res.status(401).send("Not Allowed");
-        }
-        list = await List.findByIdAndUpdate(req.params.id, { $set: newList }, { new: true })
-        
+        list = await List.findOneAndUpdate({ userId: req.user.id }, { $set: newList }, { new: true })
+
         res.json({ list });
     } catch (error) {
         console.error(error.message);
@@ -56,23 +59,25 @@ router.put('/updatelist/:id', fetchuser, async (req, res) => {
     }
 })
 
-// ROUTE 4: Delete an existing Listitem using: DELETE "/api/lists/deletelist". Login required
-router.delete('/deletelist/:id', fetchuser, async (req, res) => {
+// ROUTE 3: Delete an existing Listitem using: DELETE "/api/lists/deletelist". Login required
+router.delete('/deletelist', fetchuser, async (req, res) => {
     try {
-        // Find the list to be delete and delete it
-        let list = await List.findById(req.params.id);
-        if (!list) { return res.status(404).send("Not Found") }
+        // Find the list item to be deleted
+        const list = await List.findOne({ userId: req.user.id });
 
-        // Allow deletion only if user owns this List
-        if (list.userId.toString() !== req.user.id) {
-            return res.status(401).send("Not Allowed");
+        // Check if the list item exists
+        if (!list) {
+            return res.status(404).json({ message: "List Not Found" });
         }
 
-        list = await List.findByIdAndDelete(req.params.id)
-        res.json({ "Success": "List has been deleted", list: list });
+        // Delete the list item
+        await List.deleteOne({ userId: req.user.id });
+
+        res.json({ message: "List has been deleted" });
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Internal Server Error");
     }
-})
+});
+
 module.exports = router
